9dd4d37df05fd9018604a59d5d952e1b09d86870
Anymok <arthur.heude@laposte.net>
2023-01-14 23:25:19 +0100

    Ajout langue hongroise/Restructuration classe Expressions

--- OHCE/Expressions.cs
+++ OHCE/Expressions.cs
@@ -10,17 +10,26 @@ namespace OHCE
     {
         public static class Français
         {
-            public const string BienDit = "Bien dit";
             public const string Bonjour = "Bonjour";
-            public const string AuRevoir = "Au revoir";
             public const string Bonsoir = "Bonsoir";
+            public const string AuRevoir = "Au revoir";
+            public const string BienDit = "Bien dit !";
         }
 
         public static class English
         {
-            public const string BienDit = "Well said";
-            public const string Bonjour = "Hello";
+            public const string Bonjour = "Good Morning";
+            public const string Bonsoir = "Good Evening";
             public const string AuRevoir = "Goodbye";
+            public const string BienDit = "Well said !";
+        }
+
+        public static class Hungarian
+        {
+            public const string Bonjour = "Helló";
+            public const string Bonsoir = "Jó estét";
+            public const string AuRevoir = "Viszlát";
+            public const string BienDit = "Jól van mondva !";
         }
     }
 }

1abda0818b555c3a84cc0c71dad7b0063209b1c9
Anymok <arthur.heude@laposte.net>
2023-01-14 23:53:22 +0100

    Ajout langue hongroises dans "Langues"

---
+++ OHCE/Langues/LangueHongroise.cs
@@ -0,0 +1,28 @@
+﻿namespace OHCE.Langues
+{
+    public class LangueHongroise : ILangue
+    {
+        /// <inheritdoc />
+        public string BienDit => Expressions.Hungarian.BienDit;
+
+        /// <inheritdoc />
+        public string DireBonjour(PériodeJournée moment)
+        {
+            return moment == PériodeJournée.Matin ? Expressions.Hungarian.Bonjour
+                : moment == PériodeJournée.AprèsMidi ? Expressions.Hungarian.Bonjour
+                : moment == PériodeJournée.Soir ? Expressions.Hungarian.Bonsoir
+                : moment == PériodeJournée.Nuit ? Expressions.Hungarian.Bonsoir
+                : Expressions.Hungarian.Bonjour;
+        }
+
+        /// <inheritdoc />
+        public string DireAuRevoir(PériodeJournée moment)
+        {
+            return moment == PériodeJournée.Matin ? Expressions.Hungarian.AuRevoirMatin
+                : moment == PériodeJournée.AprèsMidi ? Expressions.Hungarian.AuRevoirAprèsMidi
+                : moment == PériodeJournée.Soir ? Expressions.Hungarian.AuRevoirSoir
+                : moment == PériodeJournée.Nuit ? Expressions.Hungarian.AuRevoirNuit
+                : Expressions.Hungarian.AuRevoir;
+        }
+    }
+}

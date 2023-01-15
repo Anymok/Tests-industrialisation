6b39856dee9961a646da4e7350ea0d50d6d856b3
Anymok <arthur.heude@laposte.net>
2023-01-15 01:32:25 +0100

    Ajout Classe SystemTImePériodeJournéeAdapter

---
+++ OHCE.Console/components/SystemTimePériodeJournéeAdapter.cs
@@ -0,0 +1,17 @@
+﻿namespace OHCE.Console
+{
+    internal class SystemTimePériodeJournéeAdapter
+    {
+        public static PériodeJournée PériodeActuelle()
+        {
+            int HeureAcutelle = DateTime.Now.Hour;
+
+            return HeureAcutelle >= 6 && HeureAcutelle <= 12 ? PériodeJournée.Matin
+                : HeureAcutelle > 12 && HeureAcutelle <= 17 ? PériodeJournée.AprèsMidi
+                : HeureAcutelle > 17 && HeureAcutelle <= 22 ? PériodeJournée.Soir
+                : HeureAcutelle > 22 || HeureAcutelle < 6 ? PériodeJournée.Nuit
+                : PériodeJournée.Defaut;
+
+        }
+    }
+}

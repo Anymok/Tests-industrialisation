1ffc3704bf9830a9fd239926da2a80607f295771
Anymok <arthur.heude@laposte.net>
2023-01-15 01:48:48 +0100

    Modif classe SystemLangProxy.cs (plus de condition)

--- OHCE.Console/components/SystemLangProxy.cs
+++ OHCE.Console/components/SystemLangProxy.cs
@@ -18,25 +18,33 @@ namespace OHCE.Console
         {
             if (locallangue == "fr-FR")
             {
-                return PériodeActuelle == PériodeJournée.Soir ? Expressions.Français.Bonsoir
-                    :PériodeActuelle == PériodeJournée.Nuit ? Expressions.Français.Bonsoir
+                return PériodeActuelle == PériodeJournée.Matin ? Expressions.Français.Bonjour
+                    : PériodeActuelle == PériodeJournée.AprèsMidi ? Expressions.Français.Bonjour
+                    : PériodeActuelle == PériodeJournée.Soir ? Expressions.Français.Bonsoir
+                    : PériodeActuelle == PériodeJournée.Nuit ? Expressions.Français.Bonsoir
                     : Expressions.Français.Bonjour;
             }
             else if (locallangue == "en-US")
             {
-                return PériodeActuelle == PériodeJournée.Soir ? Expressions.English.Bonsoir
+                return PériodeActuelle == PériodeJournée.Matin ? Expressions.English.Bonjour
+                    : PériodeActuelle == PériodeJournée.AprèsMidi ? Expressions.English.Bonjour
+                    : PériodeActuelle == PériodeJournée.Soir ? Expressions.English.Bonsoir
                     : PériodeActuelle == PériodeJournée.Nuit ? Expressions.English.Bonsoir
                     : Expressions.English.Bonjour;
             }
             else if (locallangue == "hu-HU")
             {
-                return PériodeActuelle == PériodeJournée.Soir ? Expressions.Hungarian.Bonsoir
+                return PériodeActuelle == PériodeJournée.Matin ? Expressions.Hungarian.Bonjour
+                    : PériodeActuelle == PériodeJournée.AprèsMidi ? Expressions.Hungarian.Bonjour
+                    : PériodeActuelle == PériodeJournée.Soir ? Expressions.Hungarian.Bonsoir
                     : PériodeActuelle == PériodeJournée.Nuit ? Expressions.Hungarian.Bonsoir
                     : Expressions.Hungarian.Bonjour;
             }
             else
             {
-                return PériodeActuelle == PériodeJournée.Soir ? Expressions.Français.Bonsoir
+                return PériodeActuelle == PériodeJournée.Matin ? Expressions.Français.Bonjour
+                    : PériodeActuelle == PériodeJournée.AprèsMidi ? Expressions.Français.Bonjour
+                    : PériodeActuelle == PériodeJournée.Soir ? Expressions.Français.Bonsoir
                     : PériodeActuelle == PériodeJournée.Nuit ? Expressions.Français.Bonsoir
                     : Expressions.Français.Bonjour;
             }
@@ -48,27 +56,35 @@ namespace OHCE.Console
         {
             if (locallangue == "fr-FR")
             {
-                return PériodeActuelle == PériodeJournée.Soir ? Expressions.Français.Bonsoir
-                    : PériodeActuelle == PériodeJournée.Nuit ? Expressions.Français.Bonsoir
-                    : Expressions.Français.Bonjour;
+                return PériodeActuelle == PériodeJournée.Matin ? Expressions.Français.AuRevoirMatin
+                    : PériodeActuelle == PériodeJournée.AprèsMidi ? Expressions.Français.AuRevoirAprèsMidi
+                    : PériodeActuelle == PériodeJournée.Soir ? Expressions.Français.AuRevoirSoir
+                    : PériodeActuelle == PériodeJournée.Nuit ? Expressions.Français.AuRevoirNuit
+                    : Expressions.Français.AuRevoir;
             }
             else if (locallangue == "en-US")
             {
-                return PériodeActuelle == PériodeJournée.Soir ? Expressions.English.Bonsoir
-                    : PériodeActuelle == PériodeJournée.Nuit ? Expressions.English.Bonsoir
-                    : Expressions.English.Bonjour;
+                return PériodeActuelle == PériodeJournée.Matin ? Expressions.English.AuRevoirMatin
+                    : PériodeActuelle == PériodeJournée.AprèsMidi ? Expressions.English.AuRevoirAprèsMidi
+                    : PériodeActuelle == PériodeJournée.Soir ? Expressions.English.AuRevoirSoir
+                    : PériodeActuelle == PériodeJournée.Nuit ? Expressions.English.AuRevoirNuit
+                    : Expressions.English.AuRevoir;
             }
             else if (locallangue == "hu-HU")
             {
-                return PériodeActuelle == PériodeJournée.Soir ? Expressions.Hungarian.Bonsoir
-                    : PériodeActuelle == PériodeJournée.Nuit ? Expressions.Hungarian.Bonsoir
-                    : Expressions.Hungarian.Bonjour;
+                return PériodeActuelle == PériodeJournée.Matin ? Expressions.Hungarian.AuRevoirMatin
+                    : PériodeActuelle == PériodeJournée.AprèsMidi ? Expressions.Hungarian.AuRevoirAprèsMidi
+                    : PériodeActuelle == PériodeJournée.Soir ? Expressions.Hungarian.AuRevoirSoir
+                    : PériodeActuelle == PériodeJournée.Nuit ? Expressions.Hungarian.AuRevoirNuit
+                    : Expressions.Hungarian.AuRevoir;
             }
             else
             {
-                return PériodeActuelle == PériodeJournée.Soir ? Expressions.Français.Bonsoir
-                    : PériodeActuelle == PériodeJournée.Nuit ? Expressions.Français.Bonsoir
-                    : Expressions.Français.Bonjour;
+                return PériodeActuelle == PériodeJournée.Matin ? Expressions.Français.AuRevoirMatin
+                    : PériodeActuelle == PériodeJournée.AprèsMidi ? Expressions.Français.AuRevoirAprèsMidi
+                    : PériodeActuelle == PériodeJournée.Soir ? Expressions.Français.AuRevoirSoir
+                    : PériodeActuelle == PériodeJournée.Nuit ? Expressions.Français.AuRevoirNuit
+                    : Expressions.Français.AuRevoir;
             }
 
         }
